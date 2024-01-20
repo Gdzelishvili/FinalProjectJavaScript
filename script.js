f957d1b91f64b3c18b1b82e3dce06ac30eb0713f
Lasha <grdzelishvili.lasha@yahoo.com>
2024-01-20 16:06:22 +0400

    finished slider and adding sing in page

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// burger bar
+
 let burger = document.querySelector(".fa-bars")
 let mark = document.querySelector(".fa-xmark")
 let menu = document.querySelector(".header_nav")
@@ -14,3 +16,61 @@ mark.addEventListener("click", () => {
     mark.style.display = "none";
     menu.classList.remove("show");
 });
+
+// slider
+
+let indexOfSlide = 1;
+
+function moveSlides(number) {
+    slidesShower( indexOfSlide += number)
+}
+
+function currentSlide(number) {
+    slidesShower(indexOfSlide = number);
+}
+
+function slidesShower(number) {
+    let i;
+    let slides = document.getElementsByClassName("myPictures");
+    let dots = document.getElementsByClassName("dot");
+
+    if (number > slides.length) {
+        indexOfSlide = 1;
+    }
+
+    if (number < 1) {
+        indexOfSlide = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+
+    slides[indexOfSlide-1].style.display = "block";
+    dots[indexOfSlide-1].className += " active";
+}
+
+slidesShower(1);
+
+
+// singin 
+
+function validateSignIn() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var errorMessage = document.getElementById('errorMessage');
+
+    // Simple validation,
+    if (username === 'example' && password === 'password') {
+        // Successful sign-in
+        errorMessage.textContent = '';
+        alert('Sign in successful!');
+    } else {
+        // Failed sign-in
+        errorMessage.textContent = 'Invalid username or password';
+    }
+}
